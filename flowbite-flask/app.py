58ccca30886e432de3be0232cfd2623c4f8d0f2d
ev_b <rainbowviola3@gmail.com>
2025-04-21 20:50:56 +0300

    Some tasks

--- flowbite-flask/app.py
+++ flowbite-flask/app.py
@@ -9,5 +9,25 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/teacher-login")
+def t_log():
+    return render_template("teacher-login.html")
+
+
+@app.route("/student-login")
+def s_log():
+    return render_template("student-login.html")
+
+
+@app.route("/teacher-acc")
+def t_acc():
+    return render_template("teacher-acc.html")
+
+
+@app.route("/student-acc")
+def s_acc():
+    return render_template("student-acc.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
